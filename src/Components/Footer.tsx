b81d49e11e3bbc5532d6599d2376313c0a3126d7
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-08 21:12:16 +0530

    num

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className=' text-sm sm:text-lg mt-4 border-black flex-col p-4 flex justify-center items-center gap-4 text-blue-500 font-bold'>
       <p className="text-lg text-green-500">Contact</p>
       <a href="mailto:tusharbhatt0135@gmail.com" target="_blank" className="hover:text-blue-700" rel="noreferrer" >Gmail</a>
-      <p  className="hover:text-blue-500">+91761446649</p>
+      <p  className="hover:text-blue-500">+917617446649</p>
       <a href="https://www.linkedin.com/in/tushar-bhatt-59b64623b" rel="noreferrer" target="_blank"  className="hover:text-blue-700">LinkedIn</a>
     </footer>
   )
