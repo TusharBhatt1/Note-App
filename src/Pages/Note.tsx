d9067a50afe312dd407274fc11fc4e91166ed09a
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-08 17:24:58 +0530

    size

--- src/Pages/Note.tsx
+++ src/Pages/Note.tsx
@@ -14,17 +14,17 @@ export function Note({onDelete}: NoteProps)
     return (
     <div className="flex flex-col gap-4 p-2">
         <div className="flex justify-between mx-4">
-         <p className="font-bold text-xl">{note.title}</p>
+         <p className="font-bold text-sm sm:text-xl">{note.title}</p>
          <div className="text-sm">
          <Link to={`/${note.id}/edit`}>
-         <button className="p-1 mr-2 w-20 bg-blue-500 text-white">Edit</button>
+         <button className="p-1 mr-2 w-10 sm:w-20 bg-blue-500 text-white">Edit</button>
          </Link>
          <button 
              onClick={()=>{onDelete(note.id) 
              navigate("/home")} }
-             className="p-1 mr-2 w-20  text-red-400 border-2 border-red-200">Delete</button>
+             className="p-1 mr-2 w-14  sm:w-20  text-red-400 border-2 border-red-200">Delete</button>
          <Link to="/home">
-         <button className="p-1 border-2 w-20 border-gray text-gray">Back</button>
+         <button className="p-1 border-2 w-10 sm:w-20 border-gray text-gray">Back</button>
          </Link>
         
          </div>
