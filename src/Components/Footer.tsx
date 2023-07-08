3ba1c02c985d691b689cc5b0ddb22de2c5a9d763
Tushar Bhatt <tusharbhatt0135@gmail.com>
2023-07-08 14:27:57 +0530

    footer-margin

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 
   return (
-    <footer className=' text-sm sm:text-lg mt-20 border-black flex-col p-4 flex justify-center items-center gap-4 text-blue-500 font-bold'>
+    <footer className=' text-sm sm:text-lg mt-12 border-black flex-col p-4 flex justify-center items-center gap-4 text-blue-500 font-bold'>
       <p className="text-lg text-green-500">Contact</p>
       <a href="mailto:tusharbhatt0135@gmail.com" target="_blank" className="hover:text-blue-700" rel="noreferrer" >Gmail</a>
       <p  className="hover:text-blue-500">+91761446649</p>
